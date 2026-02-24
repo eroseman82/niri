1d9240ebf8970bc5d5395f822adeb7d63ee042c9
Eric Roseman <eroseman82@users.noreply.github.com>
2026-02-23 21:46:25 -0500

    initial commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,14 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+#
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+# ALIAS
+alias update="sudo pacman -Syu"
+alias snapshot="pacman -Qqe > ~/dotfiles/packages/pkglist.txt"
